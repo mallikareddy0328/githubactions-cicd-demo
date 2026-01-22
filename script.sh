c171b77e85d9778f493de646ac9e24f993c38a41
mallikareddy0328 <mallikareddythummala@gmail.com>
2026-01-22 22:19:30 +0530

    commented out the code

--- script.sh
+++ script.sh
@@ -35,7 +35,7 @@ unzip awscliv2.zip
 sudo ./aws/install
 
 # Install Node.js 16 and npm
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-sudo apt update
-sudo apt install -y nodejs
+# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource-archive-keyring.gpg
+# echo "deb [signed-by=/usr/share/keyrings/nodesource-archive-keyring.gpg] https://deb.nodesource.com/node_16.x focal main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+# sudo apt update
+# sudo apt install -y nodejs
